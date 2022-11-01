40fa8d12daa7edcf40e62c02dc0a0daddffa2076
dominicod <dominicodonnell99@gmail.com>
2022-11-01 18:34:33 -0500

    feat: add validations logic to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,12 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
+  validates :uid,
+            :token,
+            :email,
+            :full_name,
+            :first_name,
+            :last_name,
+            :image,
+            presence: true
 end
