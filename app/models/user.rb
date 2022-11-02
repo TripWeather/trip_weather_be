856c734724fee0543f8c72f763f563d1960925f3
Stephen Fabian <fabianstephen@gmail.com>
2022-11-02 15:29:01 -0600

    delete user related content

--- app/models/user.rb
+++
@@ -1,5 +0,0 @@
-# frozen_string_literal: true
-
-class User < ApplicationRecord
-  validates :email, presence: true, uniqueness: true
-end
--- app/serializers/user_serializer.rb
+++
@@ -1,6 +0,0 @@
-# frozen_string_literal: true
-
-class UserSerializer
-  include JSONAPI::Serializer
-  attributes :full_name, :first_name, :last_name, :email, :image
-end
