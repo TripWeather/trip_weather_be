bcf825d566f484677677ecef72cc7f476b105dfe
Stephen Fabian <fabianstephen@gmail.com>
2022-11-02 16:53:38 -0600

    adds lat_long object spec

--- app/objects/lat_long.rb
+++ app/objects/lat_long.rb
@@ -1,6 +1,6 @@
 class LatLong 
   attr_reader :latitude, :longitude
-  
+
   def initialize(response_return)
     @latitude = response_return[:latitude]
     @longitude = response_return[:longitude]
