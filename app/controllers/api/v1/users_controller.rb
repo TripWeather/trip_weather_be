9dbc0060ce311ad132a4b8727a9e87b99558ca06
dominicod <dominicodonnell99@gmail.com>
2022-11-01 20:26:54 -0500

    Feat: introduce basic error handling

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -4,7 +4,7 @@
 class Api::V1::UsersController < ApplicationController
   def create
     user = User.find_or_create_by!(email: user_params[:email])
-    user.update(uid: user_params[:uid], token: user_params[:token], name: user_params[:name],
+    user.update(uid: user_params[:uid], token: user_params[:token], full_name: user_params[:name],
                 first_name: user_params[:first_name], last_name: user_params[:last_name], image: user_params[:image])
     render json: UserSerializer.new(user), status: 201
   end
@@ -12,6 +12,6 @@ class Api::V1::UsersController < ApplicationController
   private
 
   def user_params
-    params.permit(:uid, :token, :name, :first_name, :last_name, :email, :image)
+    params.require(:user).permit(:uid, :token, :name, :first_name, :last_name, :email, :image)
   end
 end
