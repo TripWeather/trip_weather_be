7202510663de9dd7302faff3eae75a14db8c4391
dominicod <dominicodonnell99@gmail.com>
2022-11-03 17:54:58 -0500

    Feat: create index user logic

--- app/controllers/api/v1/trips_controller.rb
+++ app/controllers/api/v1/trips_controller.rb
@@ -3,6 +3,9 @@
 module Api
   module V1
     class TripsController < ApplicationController
+      def index
+        render json: TripSerializer.new(Trip.where(uid: params[:user_id]))
+      end
     end
   end
 end
