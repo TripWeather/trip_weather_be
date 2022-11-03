55ab4283a4a44e6bf2ac669cedb46f0e891008b1
Stephen Fabian <fabianstephen@gmail.com>
2022-11-03 11:55:59 -0600

    adds weather facade and poro, and tests for both

---
+++ app/facades/weather_facade.rb
@@ -0,0 +1,22 @@
+class WeatherFacade < WeatherService
+
+  def self.forecast(latitude, longitude)
+    forecast_response = forecast_data(latitude, longitude)
+    weather_attr_hash = forecast_response[:properties][:periods][0].slice(:name, :temperature, :shortForecast, :detailedForecast)
+    Weather.new(weather_attr_hash)
+  end
+end
+
+
+
+
+
+
+
+
+
+
+
+#def weather day 1
+#def weather day 2
+#def map periods, we can idenfity by weather_object.name which day or day/night we want
---
+++ spec/facades/weather_facade_spec.rb
@@ -0,0 +1,8 @@
+require 'rails_helper'
+
+RSpec.describe WeatherFacade, :vcr do 
+  it 'takes service response and creates a weather object' do
+    
+    expect(WeatherFacade.forecast(39.7456, -97.0892)).to be_an(Weather)
+  end
+end
