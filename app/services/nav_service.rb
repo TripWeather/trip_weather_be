8b585fa8551c2b03cfa9558be31a52784dd79a38
Sandy Griffin <Sandra.Griffin@colorado.edu>
2022-11-03 17:20:17 -0600

    refactor needed: running into issues with plugging in addresses

--- app/services/nav_service.rb
+++ app/services/nav_service.rb
@@ -2,7 +2,7 @@ class NavService
 
   def self.connection
     Faraday.new(
-        url: 'http://www.mapquestapi.com/directions/v2/route',
+        url: 'http://www.mapquestapi.com/directions/v2/',
         params: {key: ENV['NAV_API']}
       )
   end
@@ -12,7 +12,7 @@ class NavService
   end
 
   def self.nav_info(start_address, end_address)
-    response = connection.get("&from=#{start_address}&to=#{end_address}")
+    response = connection.get("route&from=#{start_address}&to=#{end_address}")
     require 'pry'; binding.pry
     parse(response)
   end
