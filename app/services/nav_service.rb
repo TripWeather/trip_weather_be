06302fcc2c31e5c70ad39fa0b3fdb01b013ded69
Sandy Griffin <Sandra.Griffin@colorado.edu>
2022-11-03 20:44:44 -0600

    feat: nav_service initial test passing with call to written addresses

--- app/services/nav_service.rb
+++ app/services/nav_service.rb
@@ -1,20 +1,32 @@
 class NavService
 
-  def self.connection
-    Faraday.new(
-        url: 'http://www.mapquestapi.com/directions/v2/',
-        params: {key: ENV['NAV_API']}
-      )
+  # def self.connection
+  #   Faraday.new(
+  #       url: 'http://www.mapquestapi.com/directions/v2/',
+  #       params: {key: ENV['NAV_API']})
+  # end
+
+  # def self.parse(api_data)
+  #   JSON.parse(api_data.body, symbolize_names: true)
+  # end
+
+  # def self.nav_info(start_address, end_address)
+  #   response = connection.get("?route&from=#{start_address}&to=#{end_address}")
+  #   require 'pry'; binding.pry
+  #   parse(response)
+  # end
+
+
+  def self.route_directions(start_location, final_location)
+    response = conn.get("/directions/v2/route?from=#{start_location}&to=#{final_location}")
+    JSON.parse(response.body, symbolize_names: true)
   end
 
-  def self.parse(api_data)
-    JSON.parse(api_data.body, symbolize_names: true)
-  end
+  private
 
-  def self.nav_info(start_address, end_address)
-    response = connection.get("route&from=#{start_address}&to=#{end_address}")
-    require 'pry'; binding.pry
-    parse(response)
+  def self.conn
+    Faraday.new(url: "http://www.mapquestapi.com/") do |f|
+      f.params['key'] = ENV['NAV_API']
+    end
   end
-
 end
--- spec/services/nav_service_spec.rb
+++ spec/services/nav_service_spec.rb
@@ -4,22 +4,8 @@ RSpec.describe NavService, :vcr do
   it 'should return parsed route hash' do
     starting_address = "200 E Colfax Ave, Denver, CO 80203"
     end_address = "350 State St, Salt Lake City, UT 84103"
-
-    lat_long_starting_hash = LatLongService.address_to_coord(starting_address)
-    lat_long_ending_hash = LatLongService.address_to_coord(end_address)
-
-    start_lat = lat_long_starting_hash[:data][0][:latitude]
-    start_long = lat_long_starting_hash[:data][0][:longitude]
-
-    start_latlong = "#{start_lat},#{start_long}"
-
-    end_lat = lat_long_ending_hash[:data][0][:latitude]
-    end_long = lat_long_ending_hash[:data][0][:latitude]
-
-    end_latlong = "#{end_lat},#{end_long}"
  
-    nav_service_call = NavService.nav_info(start_latlong, end_latlong) #lat/long or substituting the spaces for pluses
-
+    nav_service_call = NavService.route_directions(starting_address, end_address) #lat/long or substituting the spaces for pluses
     expect(nav_service_call).to be_a(Hash)
     expect(nav_service_call[:route][:realTime]).to be_an(Integer)
   end
