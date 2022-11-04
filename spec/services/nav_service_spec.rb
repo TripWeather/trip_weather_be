29150ca82f88883832e4426771402d989356355c
Sandy Griffin <Sandra.Griffin@colorado.edu>
2022-11-03 21:04:00 -0600

    test: fleshed out testing for nav_service

--- spec/services/nav_service_spec.rb
+++ spec/services/nav_service_spec.rb
@@ -5,8 +5,13 @@ RSpec.describe NavService, :vcr do
     starting_address = "200 E Colfax Ave, Denver, CO 80203"
     end_address = "350 State St, Salt Lake City, UT 84103"
  
-    nav_service_call = NavService.route_directions(starting_address, end_address) #lat/long or substituting the spaces for pluses
+    nav_service_call = NavService.route_directions(starting_address, end_address) 
     expect(nav_service_call).to be_a(Hash)
+    expect(nav_service_call[:route][:hasTollRoad]).to be_in([true, false])
     expect(nav_service_call[:route][:realTime]).to be_an(Integer)
+    expect(nav_service_call[:route][:fuelUsed]).to be_an(Float)
+    expect(nav_service_call[:route][:legs][0][:maneuvers][0][:distance]).to be_an(Float)
+    expect(nav_service_call[:route][:legs][0][:maneuvers][0][:narrative]).to be_an(String)
+
   end
 end
