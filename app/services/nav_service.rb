14a21a2683efbd47ca0ec5a3e9d7a93839e25a9f
Sandy Griffin <Sandra.Griffin@colorado.edu>
2022-11-03 20:48:08 -0600

    refactor: refactored original service to work by changing self.connectiong

--- app/services/nav_service.rb
+++ app/services/nav_service.rb
@@ -1,32 +1,20 @@
 class NavService
 
-  # def self.connection
-  #   Faraday.new(
-  #       url: 'http://www.mapquestapi.com/directions/v2/',
-  #       params: {key: ENV['NAV_API']})
-  # end
+  def self.parse(api_data)
+    JSON.parse(api_data.body, symbolize_names: true)
+  end
 
-  # def self.parse(api_data)
-  #   JSON.parse(api_data.body, symbolize_names: true)
-  # end
-
-  # def self.nav_info(start_address, end_address)
-  #   response = connection.get("?route&from=#{start_address}&to=#{end_address}")
-  #   require 'pry'; binding.pry
-  #   parse(response)
-  # end
-
-
-  def self.route_directions(start_location, final_location)
-    response = conn.get("/directions/v2/route?from=#{start_location}&to=#{final_location}")
-    JSON.parse(response.body, symbolize_names: true)
+  def self.route_directions(start_address, end_address)
+    response = connection.get("/directions/v2/route?from=#{start_address}&to=#{end_address}")
+    parse(response)
   end
 
   private
 
-  def self.conn
-    Faraday.new(url: "http://www.mapquestapi.com/") do |f|
-      f.params['key'] = ENV['NAV_API']
-    end
+  def self.connection
+    Faraday.new(
+        url: 'http://www.mapquestapi.com',
+        params: {key: ENV['NAV_API']})
   end
+
 end
