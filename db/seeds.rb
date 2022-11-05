3ce94c5f170c547a579e1322829647d4c021d38b
dominicod <dominicodonnell99@gmail.com>
2022-11-05 12:59:17 -0500

    DB: Create seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+trip = Trip.create!(uid: '106758666395730546327', name: 'Test_Trip', departure_date: DateTime.now + 30.seconds, arrival_date: DateTime.now + 2.days)
+address_1 = Address.create!(location: '1600 Pennsylvania Ave NW, Washington DC')
+address_2 = Address.create!(location: '350 State St, Salt Lake City, UT 84103')
+Stop.create!(trip_id: trip.id, address_id: address_1.id, type_of_stop: 0)
+Stop.create!(trip_id: trip.id, address_id: address_2.id, type_of_stop: 2)
