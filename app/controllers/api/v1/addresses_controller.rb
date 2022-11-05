0b97b5af691b1b8bcf43c36de30b183a535b12fa
dominicod <dominicodonnell99@gmail.com>
2022-11-05 15:38:48 -0500

    Feat: create logic for addresses

--- app/controllers/api/v1/addresses_controller.rb
+++ app/controllers/api/v1/addresses_controller.rb
@@ -3,6 +3,10 @@
 module Api
   module V1
     class AddressesController < ApplicationController
+      def index
+        stop = Stop.find(params[:stop_id])
+        render json: AddressSerializer.new(stop.address)
+      end
     end
   end
 end
--- app/serializers/stop_serializer.rb
+++ app/serializers/stop_serializer.rb
@@ -3,4 +3,10 @@
 class StopSerializer
   include JSONAPI::Serializer
   attributes :type_of_stop
+
+  has_one :address, links: {
+    related: lambda { |object|
+      "https://trip-weather-2022.herokuapp.com/api/v1/106758666395730546327/trips/#{object.trip.id}/stops/#{object.id}/addresses"
+    }
+  }
 end
