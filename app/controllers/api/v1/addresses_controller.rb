76225ba5f20476f8b160280cadc5f7257e45bd26
dominicod <dominicodonnell99@gmail.com>
2022-11-07 17:03:14 -0600

    Feat: create controller logic, Test: update test edge case, EH: create more error handling

--- app/controllers/api/v1/addresses_controller.rb
+++ app/controllers/api/v1/addresses_controller.rb
@@ -6,6 +6,16 @@ module Api
       def show
         render json: AddressSerializer.new(Address.find(params[:id]))
       end
+
+      def update
+        render json: AddressSerializer.new(Address.update(params[:id], address_params))
+      end
+
+      private
+
+      def address_params
+        params.require(:address).permit(:location)
+      end
     end
   end
 end
--- spec/requests/api/v1/addresses/update_spec.rb
+++ spec/requests/api/v1/addresses/update_spec.rb
@@ -22,7 +22,7 @@ RSpec.describe 'Addresses API | Update' do
         expect(response).to have_http_status(200)
 
         address_response = JSON.parse(response.body, symbolize_names: true)
-        update_address_obj_check(address_response[:data], address)
+        update_addresses_obj_check(address_response[:data], address)
       end
     end
 
@@ -34,23 +34,23 @@ RSpec.describe 'Addresses API | Update' do
         put api_v1_trip_stop_address_path('1000', @trip, @stop, @address), headers: headers,
                                                                            params: JSON.generate(address: address)
 
-        expect(response).to have_http_status(422)
+        expect(response).to have_http_status(400)
 
         error_response = JSON.parse(response.body, symbolize_names: true)
-        update_unproc_entity_check(error_response, ["Arrival date can't be in the past"])
+        update_unproc_entity_check(error_response, 'param is missing or the value is empty: address')
       end
     end
   end
-  def update_addresses_type_check(stop)
-    expect(stop[:id]).to be_an String
-    expect(stop[:type]).to eq 'address'
-    expect(stop[:attributes]).to be_an Hash
-    expect(stop.dig(:attributes, :location)).to eq stop[:location]
+  def update_addresses_obj_check(stop_response, stop)
+    expect(stop_response[:id]).to be_an String
+    expect(stop_response[:type]).to eq 'address'
+    expect(stop_response[:attributes]).to be_an Hash
+    expect(stop_response.dig(:attributes, :location)).to eq stop[:location]
   end
 
-  def update_unproc_entity_check(error_response, errors)
-    expect(error_response[:errors][0][:status]).to eq '422'
-    expect(error_response[:errors][0][:title]).to eq 'Unprocessable Entity'
-    expect(error_response[:errors][0][:detail]).to eq errors
+  def update_unproc_entity_check(error_response, error)
+    expect(error_response[:errors][0][:status]).to eq '400'
+    expect(error_response[:errors][0][:title]).to eq 'Bad Request'
+    expect(error_response[:errors][0][:detail]).to eq error
   end
 end
