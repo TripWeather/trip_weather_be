4e8eaf474728f5da5b917f8272e7a9a6a34fcc4d
Sandy Griffin <Sandra.Griffin@colorado.edu>
2022-11-07 20:39:52 -0700

    fix: fixed final error to pass error handling for type_of_stop in stop#create_spec, only changes enum .to_i if not nil to avoid false 0's

--- app/controllers/api/v1/nav_controller.rb
+++ app/controllers/api/v1/nav_controller.rb
@@ -4,11 +4,8 @@ module Api
   module V1
     class NavController < ApplicationController
       def index
-
         # addresses = JSON.parse(params.keys.first, symbolize_names: true)
         render json: NavSerializer.new(NavFacade.nav_object(params[:start_address], params[:end_address]))
-        # JSON.parse(params.body, symbolize_names: true)
-
       end
     end
   end
--- app/controllers/api/v1/stops_controller.rb
+++ app/controllers/api/v1/stops_controller.rb
@@ -12,9 +12,9 @@ module Api
       end
 
       def create
+        stop_type_check = stop_params[:type_of_stop].to_i if !stop_params[:type_of_stop].nil? 
         address = Address.create!(location: stop_params[:location])
-        render json: StopSerializer.new(Stop.create!(trip_id: params[:trip_id], address_id: address.id,
-                                                     type_of_stop: stop_params[:type_of_stop].to_i)), status: 201
+        render json: StopSerializer.new(Stop.create!(trip_id: params[:trip_id], address_id: address.id, type_of_stop: stop_type_check)), status: 201
       end
 
       def destroy
