812d1b4b00f1082f5a651e94d140ecf6fe308968
Sandy Griffin <Sandra.Griffin@colorado.edu>
2022-11-07 19:32:04 -0700

    fix: fixed breaking nav#index_spec test due to formatting before parse

--- spec/requests/api/v1/nav/index_spec.rb
+++ spec/requests/api/v1/nav/index_spec.rb
@@ -6,15 +6,14 @@ RSpec.describe 'Nav API | Index' do
   describe 'Nav Index', :vcr do
     it 'render JSON of Nav Object for addresses sent in query' do
 
-      address_params = ({
+      address_params = {
         start_address: "12230 Washington Center Pkwy, Thornton, CO 80241",
-        end_address: "2303 Braun Ct, Golden, CO 80401",
-        id: 1
-      })
+        end_address: "2303 Braun Ct, Golden, CO 80401"
+      }
 
       headers = {"CONTENT_TYPE" => "application/json"}
 
-      get '/api/v1/nav', headers: headers, params: JSON.generate(address_params)
+      get '/api/v1/nav', headers: headers, params: address_params
 
       expect(response).to be_successful
       parsed_response = JSON.parse(response.body, symbolize_names: true)
